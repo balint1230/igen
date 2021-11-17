f1316dc538e48e24ddee7342bd91e9bf0c81da2f
balint1230 <82216390+balint1230@users.noreply.github.com>
2021-11-17 10:49:54 +0100

    Add files via upload

---
+++ gyak.py
@@ -0,0 +1,8 @@
+nevek=['Szandi','Ferenc','Ganriella','Bálint'']
+adat=print(str(input("Adj meg egy nevet"))
+
+if adat in nevek:
+    print(adat)
+else:
+    print("Nem található ilyen név a listában.")
+
